099aaf099d7567b0ba34ea009d1b49e3e26493f5
DominicDev <s0l0estudi4r@gmail.com>
2025-08-28 22:54:46 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' EVA AYLLÓN ';
+export let nameSongs = [
+	"QUE SOMOS AMANTES", /*1*/
+	"MAL PASO", /*2*/
+	"SACA LAS MANOS", /*3*/
+	"HUYE DE MI", /*4*/
+	"NADA SOY", /*5*/
+	"MI CARIÑITO", /*6*/
+	"YO PERDÍ EL CORAZÓN", /*7*/
+	"AMORCITO ", /*8*/
+	"PERDISTE ", /*9*/
+	"SI SI", /*10*/
+	"NADA TE RECLAMO", /*11*/
+	"CARIÑO MALO", /*12*/
+	"SOMBRA", /*13*/
+	"MARUJITA", /*14*/
+	"PROPRIEDAD PRIVADA", /*15*/
+	"DE QUE ESTOY HECHA", /*16*/
+	"LE DIJE A PAPA", /*17*/
+	"RITMO COLOR Y SABOR", /*18*/
+	"INGA", /*19*/
+	"ROSA TE", /*20*/
+]
